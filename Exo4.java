10ca52fda03b30449ca22964da1c86b2b8811c94
CocoChocotte <constance.maillard@ynov.com>
2017-10-17 09:15:28 +0200

    Update Exo4.java

--- Exo4.java
+++ Exo4.java
@@ -1,40 +1,29 @@
 package exercice4;
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Exo4 {
-
+public class TestExo4 {
     public static void main(String[] args) {
-
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Veuillez saisir un chemin d'acces:");
         String entree = scan.nextLine();
-
-        File f = new File (entree);
-
-
-        try
-        {
-            FileWriter fw = new FileWriter (f);
-
+        File f = new File(entree);
+        try {
+            FileWriter fw = new FileWriter(f, true);
             Scanner donneAjoute = new Scanner(System.in);
             System.out.println("Que voulez vous ajouter a votre fichier ? ");
-            String ajout = donneAjoute.next();
-
-           while (ajout.equals("quit") )
-            {
-                fw.write (String.valueOf (ajout));
-                fw.write ("\r\n");
-                ajout = donneAjoute.next();
+            String ajout = donneAjoute.nextLine();
+            while (!ajout.equals("quit")) {
+                fw.write(ajout);
+                fw.write("\r\n");
+                ajout = donneAjoute.nextLine();
             }
 
             fw.close();
-        }
-        catch (IOException exception)
-        {
-            System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+        } catch (IOException exception) {
+            System.out.println("Erreur lors de la lecture : " + exception.getMessage());
         }
     }
-
 }
